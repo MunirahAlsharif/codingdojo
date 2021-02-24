5f7b6f03a83284f9e8a4f224db2aaffd35a10327
MunirahAlsharif <78304610+MunirahAlsharif@users.noreply.github.com>
2021-02-25 02:09:31 +0300

    Add files via upload

--- Week_4/Insurance.sol
+++ Week_4/Insurance.sol
@@ -1,6 +1,10 @@
+//SPDX-License-Identifier: MIT
+
 pragma solidity >=0.7.0 <0.8.0;
 
-contract TechInsurance {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract TechInsurance is ERC721 {
     
     /** 
      * Defined two structs
@@ -32,21 +36,23 @@ contract TechInsurance {
  
     function addProduct(uint _productId, string memory _productName, uint _price ) public {
        Product memory NewProduct = Product (_productId , _productName , _price , true);
+       insOwner [msg.sender]= NewProduct;
        productIndex [productCounter ++] = NewProduct;
  
     }
     
     
     function changeFalse(uint _productIndex) public {
-
+       require (msg.value == productIndex[_productIndex]).changeFalse[offered]=false;
     }
     
     function changeTrue(uint _productIndex) public {
+       require (msg.value == productIndex[_productIndex]).changeTrue[offered]=true;
 
     }
     
     function changePrice(uint _productIndex, uint _price) public {
-
+       require (msg.value == productIndex[_productIndex].changePrice=_price);
     }
     
     function clientSelect(uint _productIndex) public payable {
