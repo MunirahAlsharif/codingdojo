7184d8c614c35d4e3e36fa628b19f98d42aead08
MunirahAlsharif <78304610+MunirahAlsharif@users.noreply.github.com>
2021-02-25 01:28:46 +0300

    Add files via upload

---
+++ Week_4/ERC20.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.7.0 < 0.8.1;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    
+    constructor () public ERC20("dojo","DOJO20"){
+       // _mint(msg.sender, 1000000000 *(10 ** (decimals())));
+        
+    }
+}
+
