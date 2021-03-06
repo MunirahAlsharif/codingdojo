1c17f488622457031c4fa2b87bac939b71c48c41
MunirahAlsharif <78304610+MunirahAlsharif@users.noreply.github.com>
2021-03-07 00:13:06 +0300

    Update InsuranceSmartContract.sol

--- Week_4/InsuranceSmartContract.sol
+++ Week_4/InsuranceSmartContract.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.1;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 /**
@@ -27,60 +28,71 @@ contract TechInsurance is ERC721("NinjaToekn","Ninja") {
         uint price;
         bool offered;
     }
-     
     struct Client {
         bool isValid;
         uint time;
     }
-    
-    
+    mapping(address => uint256) public _balances;
     mapping(uint => Product) public productIndex;
     mapping(address => mapping(uint => Client)) public client;
     
     uint productCounter;
-
-    address payable insOwner;
-    constructor(address payable _insOwner) public{
-        insOwner = _insOwner;
-    }
+    address  insOwner;
+    constructor() public{
+    insOwner = msg.sender;}
  
     function addProduct(uint _productId, string memory _productName, uint _price ) public {
         Product memory NewProduct = Product (_productId , _productName , _price , true);
         productIndex [productCounter ++] = NewProduct;
     }
-    
-    
+     function mint(address to , uint256 tokenId)public  {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+        _beforeTokenTransfer(address(0), to, tokenId);
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
     function doNotOffer(uint _productIndex) public returns(bool) {
         require(msg.sender == insOwner,"No Offer");
         return productIndex[_productIndex].offered = false;
-
     }
-    
     function forOffer(uint _productIndex) public returns(bool) {
         require(msg.sender == insOwner,"Yes Offer");
         return productIndex[_productIndex].offered = true;
 
     }
-    
     function changePrice(uint _productIndex, uint _price) public view {
         require( productIndex[_productIndex].price>=1);
         productIndex[_productIndex].price==_price;
     }
-    
     /**
     *@dev 
     * Every client buys an insurance, 
     * you need to map the client's address to the id of product to struct client, using (client map)
     */
-    
     function buyInsurance(uint _productIndex) public payable {
-        require(productIndex[_productIndex].price == msg.value);
+        require(productIndex[_productIndex].price == msg.value ,"Not correct");
         Client memory NewClient;
         NewClient.isValid=true;
+        NewClient.time = block.number;
         client[msg.sender][_productIndex]= NewClient;
-        insOwner.transfer(msg.value);
-        
+        uint price= msg.value;
+        payable(msg.sender).transfer(msg.value);
         
     } 
+    function balanceOf(address insOwner) public view override returns(uint256) {
+        require(insOwner != address(0));
+        return _balances[insOwner];
+        
+    }
+    
+    function transferID( address to, uint256 tokenId) public  {
+        //solhint-disable-next-line max-line-length
+        require(ownerOf(tokenId) == msg.sender,"only owner" );
+        require(to != address(0), "address to not 0 ");
+        _transfer(msg.sender, to, tokenId);
+    }
     
 }
